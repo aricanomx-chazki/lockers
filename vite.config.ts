42baa5584bfd12b1ac617988d3d6b1a32252ded5
Andrés Ricaño <aricanomx@gmail.com>
2023-10-23 15:56:42 -0600

    [Fix]
    message: remove /lockers form base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Lockers-demo/',
+  base: '/',
   resolve: {
     alias: {
       src: '/src',
